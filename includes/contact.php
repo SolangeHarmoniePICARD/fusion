9b52a6afb73fe4e43c1266852b189ef05e56842c
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-07-25 14:04:38 +0200

    Responsivité + compatibilité Firefox

--- includes/contact.php
+++ includes/contact.php
@@ -8,30 +8,30 @@
     </div>
 
   <div class="row flex-grow-1 justify-content-center align-items-center mt-5">
-    <div class="col-8">
+    <div class="col-sm-12 col-md-8">
     <form class="p-5 bg-black shadow-lg">
       <div class="form-row">
         <div class="field  col-md-6">
-          <input type="text" class="bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputName" placeholder="Solange Picard" required>
+          <input type="text" class="bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputName" placeholder="&nbsp;" required>
           <label for="inputName" class="text-white text-uppercase">Your name</label>
         </div>
         <div class="field  col-md-6">
-          <input type="email" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputEmail" placeholder="solange.picard@cryptolab.net" required>
+          <input type="email" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputEmail" placeholder="&nbsp;" required>
           <label for="inputEmail" class="text-uppercase text-white">Your email</label>
         </div>
       </div>
       <div class="form-row">
         <div class="field  col-md-6">
-          <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputCompagny" placeholder="Self Employed" required>
+          <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputCompagny" placeholder="&nbsp;" required>
           <label for="inputCompagny" class="text-white text-uppercase">Compagny</label>
         </div>
         <div class="field  col-md-6">
-          <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputSubject" placeholder="About you" required>
+          <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputSubject" placeholder="&nbsp;" required>
           <label for="inputSubject" class="text-white text-uppercase">Subject</label>
         </div>
       </div>
       <div class="field ">
-        <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputMessage" placeholder="My message" required>
+        <input type="text" class=" bg-black border-top-0 border-right-0 border-left-0 rounded-0" id="inputMessage" placeholder="&nbsp;" required>
         <label for="inputMessage" class="text-white text-uppercase">Your message</label>
       </div>
       <div class="text-center">
@@ -44,19 +44,19 @@
 
 <footer class="container mt-5">
   <div class="row">
-  <div class="col-4 card-block mb-5">
+  <div class="col-sm-12 col-md-4 card-block mb-5">
     <h5 class="card-title text-white">California office</h5>
     <p class="card-text text-secondary">4243 Woodland Terrace</p>
     <p class="card-text text-secondary">Sacramento, CA 95814</p>
     <p class="card-text text-secondary">916 753 2645</p>
   </div>
-  <div class="col-4 card-block mb-5">
+  <div class="col-sm-12 col-md-4 card-block mb-5">
     <h5 class="card-title text-white">New York office</h5>
     <p class="card-text text-secondary">885 Oakwood Avenue</p>
     <p class="card-text text-secondary">New York, NY 10013</p>
     <p class="card-text text-secondary">212 660 0744</p>
   </div>
-  <div class="col-4 card-block mb-5">
+  <div class="col-sm-12 col-md-4 card-block mb-5">
     <h5 class="card-title text-white">Other contact</h5>
     <p class="card-text text-secondary">solange.picard@cryptolab.net</p>
     <p class="card-text">
--- includes/team.php
+++ includes/team.php
@@ -12,22 +12,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right border-bottom text-center flip-card border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <img src="https://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
               <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <p class="card-text text-white mb-5">Chief Technical Officer</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Solange PICARD</span> <br> <small>Chief Technical Officer</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -39,22 +39,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right border-bottom text-center flip-card border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://solangeharmoniepicard.fr/fusion/img/team/aminata1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Aminata DIOP</h5>
+              <p class="card-text text-white mb-5">Front-End Developer</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/team/aminata1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Aminata DIOP</span> <br> <small>Front-End Developer</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -66,22 +66,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right border-bottom text-center flip-card border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://promo-21.codeur.online/blog/wp-content/uploads/2018/12/adji1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Adji Fatou DIEYE</h5>
+              <p class="card-text text-white mb-5">Front-End Developer</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://promo-21.codeur.online/blog/wp-content/uploads/2018/12/adji1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Adji Fatou DIEYE</span> <br> <small>Front-End Developer</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -93,22 +93,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-bottom text-center flip-card border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://solangeharmoniepicard.fr/fusion/img/team/adama1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Adama AIDARA</h5>
+              <p class="card-text text-white mb-5">Community Manager</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/team/adama1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Adama AIDARA</span> <br> <small>Community Manager</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -128,22 +128,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right text-center flip-card mb-5 border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://promo-21.codeur.online/blog/wp-content/uploads/2018/12/ndeye1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Ndeye LEYE</h5>
+              <p class="card-text text-white mb-5">Back-End Developer</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://promo-21.codeur.online/blog/wp-content/uploads/2018/12/ndeye1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Ndeye LEYE</span> <br> <small>Back-End Developer</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -155,22 +155,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right text-center flip-card mb-5 border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://solangeharmoniepicard.fr/fusion/img/team/oumou1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Oumou SENE</h5>
+              <p class="card-text text-white mb-5">Back-End Developer</p>
             </div>
             <div class="flip-card-back text-left  mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/team/oumou1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Oumou SENE</span> <br> <small>Back-End Developer</small>
                     </div>
                     </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -182,22 +182,22 @@
         <div class="col-sm-12 col-md-3 card-block border-secondary border-right text-center flip-card mb-5 border-color-flip">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://solangeharmoniepicard.fr/fusion/img/team/aissatou1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Aïssatou NDAO</h5>
+              <p class="card-text text-white mb-5">Webdesigner</p>
             </div>
             <div class="flip-card-back text-left  mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/team/aissatou1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                        <span class="text-warning">Aïssatou NDAO</span> <br> <small>Webdesigner</small>
+                      </div>
                     </div>
-                    </div>
-                  <div class="col-sm-12 col-md-9 card-text text-justify">
+                  <div class="col-sm-12 col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>
@@ -209,22 +209,22 @@
         <div class="col-sm-12 col-md-3 card-block text-center flip-card mb-5">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
-              <h5 class="card-title text-warning">Solange PICARD</h5>
-              <p class="card-text text-white mb-5">Developer</p>
+              <img src="https://solangeharmoniepicard.fr/fusion/img/team/fatoumata1.png" class="card-img-top img-circle mt-5 mb-3" alt="Photo d'identité">
+              <h5 class="card-title text-warning">Fatoumata TOURE</h5>
+              <p class="card-text text-white mb-5">Marketing & communication</p>
             </div>
             <div class="flip-card-back text-left mt-4">
               <div class="container-fluid">
                 <div class="row">
-                  <div class="col-sm-12 col-md-3 col-flip-img">
-                    <div class="border-warning border-left border-top mb-3 col-flip-img">
-                      <img src="http://solangeharmoniepicard.fr/fusion/img/profil.jpg" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
+                  <div class="col-sm-12 col-md-4 flip-flex">
+                    <div class="border-warning border-left border-top mb-3">
+                      <img src="https://solangeharmoniepicard.fr/fusion/img/team/fatoumata1.png" class="flip-img mb-1 mt-1 ml-1" alt="Photo d'identité">
                     </div>
-                    <div class="card-title flip-card-left-text col-flip-img">
-                      <span class="text-warning">Solange PICARD</span> <br> <small>Developer</small>
+                    <div class="card-title flip-card-left-text ml-2">
+                      <span class="text-warning">Fatoumata TOURE</span> <br> <small>Marketing & communication</small>
                     </div>
                     </div>
-                  <div class="col-md-9 card-text text-justify">
+                  <div class="col-md-8 card-text text-justify">
                     <p class="flip-card-text-size">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
                   </div>
                 </div>

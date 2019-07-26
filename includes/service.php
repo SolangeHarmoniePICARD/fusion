66691325588fe29561b1df0b8df08de32125576e
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-07-26 12:36:42 +0200

    Ajout du SmoothScroll en CSS...

--- includes/service.php
+++ includes/service.php
@@ -10,21 +10,24 @@
       <div class="col-sm-12 col-md-4 card-block mb-5">
         <i class="fas fa-file-alt text-warning h2 mb-3"></i>
         <h5 class="card-title text-white">Strategy.</h5>
-        <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.</p>
         <p class="card-text text-white text-uppercase">See details <i class="fas fa-long-arrow-alt-right text-warning"></i></p>
       </div>
       <div class="col-sm-12 col-md-4 card-block mb-5">
         <i class="fas fa-pen-fancy text-warning h2 mb-3"></i>
         <h5 class="card-title text-white">Branding.</h5>
-        <p class="card-text text-secondary">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p class="card-text text-white text-uppercase">See details <i class="fas fa-long-arrow-alt-right text-warning"></i></p>
+        <p class="card-text text-secondary">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.</p>
+        <p class="card-text text-white text-uppercase">See details <i class="fas fa-long-arrow-alt-right text-warning"></i></p>
       </div>
       <div class="col-sm-12 col-md-4 card-block mb-5">
         <i class="fas fa-pencil-ruler text-warning h2 mb-3"></i>
         <h5 class="card-title text-white">Design.</h5>
-        <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p class="card-text text-white text-uppercase">See details <i class="fas fa-long-arrow-alt-right text-warning"></i></p>
-      </div>
+        <p class="card-text text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.</p>
+        <p class="card-text text-white text-uppercase">See details <i class="fas fa-long-arrow-alt-right text-warning"></i></p>
       </div>
     </div>
+  </div>
 </section>

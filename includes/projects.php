5fc07b0bd31164351bad9a2e138b170029716bc1
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-16 10:59:13 +0200

    Liens cassés suite à la mise en place du .htaccess

--- includes/projects.php
+++ includes/projects.php
@@ -8,7 +8,7 @@
           <p class="display-4 text-white font-weight-bold no-wrap">Project One</p>
         </div>
         <div class="col-8 mt-5 mb-5">
-          <img class="img-fluid animated fadeIn" src="http://solangeharmoniepicard.fr/img/solange-harmonie-picard-005.jpg" style="width:100%">
+          <img class="img-fluid animated fadeIn" src="https://solangeharmoniepicard.fr/img/solange-harmonie-picard-005.jpg" style="width:100%">
         </div>
         <div class="col-2 mt-5 mb-5 d-flex align-items-start flex-column">
           <p class="text-white mt-auto mb-0"><a class="text-white text-decoration-none" href="#"><span class="align-middle text-uppercase"><small>Project details <i class="fas fa-long-arrow-alt-right text-warning"></i></span></small></a></p>
@@ -25,7 +25,7 @@
           <p class="display-4 text-white font-weight-bold no-wrap">Project Two</p>
         </div>
         <div class="col-8 mt-5 mb-5">
-          <img class="img-fluid animated fadeIn" src="http://solangeharmoniepicard.fr/img/solange-harmonie-picard-004.jpg" style="width:100%">
+          <img class="img-fluid animated fadeIn" src="https://solangeharmoniepicard.fr/img/solange-harmonie-picard-004.jpg" style="width:100%">
         </div>
         <div class="col-2 mt-5 mb-5 d-flex align-items-start flex-column">
           <p class="text-white mt-auto mb-0"><a class="text-white text-decoration-none" href="#"><span class="align-middle text-uppercase"><small>Project details <i class="fas fa-long-arrow-alt-right text-warning"></i></span></small></a></p>
@@ -42,7 +42,7 @@
           <p class="display-4 text-white font-weight-bold no-wrap">Project Three</p>
         </div>
         <div class="col-8 mt-5 mb-5">
-          <img class="img-fluid animated fadeIn" src="http://solangeharmoniepicard.fr/img/solange-harmonie-picard-037.jpg" style="width:100%">
+          <img class="img-fluid animated fadeIn" src="https://solangeharmoniepicard.fr/img/solange-harmonie-picard-037.jpg" style="width:100%">
         </div>
         <div class="col-2 mt-5 mb-5 d-flex align-items-start flex-column">
           <p class="text-white mt-auto mb-0"><a class="text-white text-decoration-none" href="#"><span class="align-middle text-uppercase"><small>Project details <i class="fas fa-long-arrow-alt-right text-warning"></i></span></small></a></p>
